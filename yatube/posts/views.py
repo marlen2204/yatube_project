bb10efa39fd355c1d9f27de0771a8bed530f0189
Марина Слобожанинова <slobozhaninovaw@gmail.com>
2022-10-12 21:08:22 +0300

    Доработаны модели, создана и настроена админка, доработаны шаблоны

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
     template = 'posts/index.html'
-    text = 'Информация на главной странице'
-    context ={
-        'text': text,
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
     }
     return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    text = 'Информация о группах'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': text,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
