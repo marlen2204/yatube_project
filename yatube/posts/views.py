0f8f569b05eb30af5fe06f31f5c69e5af409e2d1
Марина Слобожанинова <slobozhaninovaw@gmail.com>
2022-10-12 13:56:53 +0300

    Добавление static

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Информация на главной странице'
+    context ={
+        'text': text,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Информация о группах'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
