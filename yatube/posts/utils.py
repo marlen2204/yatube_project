3859cc7a3e8ba9ad7175da78bfd3f5058c128eb9
Марина Слобожанинова <slobozhaninovaw@gmail.com>
2022-10-31 00:21:13 +0300

    Доработана вью функция для редактирования поста, паджинация вынесена в отдельный фaйл utils, оптимизированы запросы к базе данных с помощью related name

---
+++ yatube/posts/utils.py
@@ -0,0 +1,9 @@
+from django.core.paginator import Paginator
+from django.conf import settings
+
+
+def make_paginator(request, data):
+    paginator = Paginator(data, settings.NUMBER_POSTS_ON_ONE_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
